e4f910e00e76050f8df593e387d6e2e57a8373d0
PriyadarshiniJR <priyadarshinijr@gmail.com>
2017-09-06 00:07:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -18,7 +18,7 @@ button.onclick = function(){
 
 
 var nameInput = document.getElementById('name');
-//var name = nameInput.value;
+var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 
 submit.onclick = function(){
@@ -27,7 +27,8 @@ submit.onclick = function(){
     request.onreadystatechange = function(){
         if(request.readyState === XMLHttpRequest.DONE){
             if(request.status === 200){
-                var names = ['name1','name2','name3'];
+                var names = request.responseText;
+                names = JSON.parse(names);
                 var list = '';
                 for(var i=0; i<names.length;i++){
                     list+='<li>'+names[i]+'</li>';
