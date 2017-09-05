dfc6fe5638db828d0de707ba109e16ff99891d78
PriyadarshiniJR <priyadarshinijr@gmail.com>
2017-09-05 23:40:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -18,5 +18,5 @@ button.onclick = function(){
     counter = counter + 1;
     var span = document.getElementById('count');
     span.innerHTML = counter.toString();
-    console.log(span.innerHTML);
+    console.log(counter);
 }
