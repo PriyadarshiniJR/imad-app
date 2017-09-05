b8adacd65f5a61b9e8637d9c577e2106786c7f64
PriyadarshiniJR <priyadarshinijr@gmail.com>
2017-09-06 00:09:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,8 +17,7 @@ button.onclick = function(){
 }
 
 
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+
 var submit = document.getElementById('submit_btn');
 
 submit.onclick = function(){
@@ -39,11 +38,8 @@ submit.onclick = function(){
             }
         }
     }
-    
+    var nameInput = document.getElementById('name');
+    var name = nameInput.value;
     request.open('GET','http://priyadarshinijr.imad.hasura-app.io/submit-name?name='+ name,true);
     request.send(null);
-    
-    
-    
-    
 }
