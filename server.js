458a171ed476ce5156263d152300ec4e53c1bebd
PriyadarshiniJR <priyadarshinijr@gmail.com>
2017-09-07 17:10:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -108,6 +108,7 @@ app.get('/check-login',function(req, res){
 
 app.get('/logout',function(req, res){
    delete req.session.auth;
+   res.send("You are logged out.");
 });
 
 var pool = new Pool(config);
