08fbe3af972ce405d9b88a583802221e54c4e824
PriyadarshiniJR <priyadarshinijr@gmail.com>
2017-09-07 11:46:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -67,7 +67,6 @@ app.get('/submit-name',function(req, res){
    res.send(JSON.stringify(names));
 });
 
-
 app.get('/articles/:articleName',function(req, res){
    
    pool.query("SELECT * FROM article WHERE title = $1", [req.params.articleName], function(err,result){
